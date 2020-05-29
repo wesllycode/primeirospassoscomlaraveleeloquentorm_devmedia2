ad2bbc7f912e5daeb74ca4b7a5a55d2f7291f700
pwcodigo <pwcodigo@gmail.com>
2020-05-29 20:01:26 -0300

    feat: Adicionado atributos no migrations imovel e no controller Imovel

--- app/database/migrations/2020_05_28_081743_create_imovels_table.php
+++ app/database/migrations/2020_05_28_081743_create_imovels_table.php
@@ -13,8 +13,27 @@ class CreateImovelsTable extends Migration
      */
     public function up()
     {
-        Schema::create('imovels', function (Blueprint $table) {
-            $table->id();
+        Schema::create('imoveis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('descricao');
+            $table->string('empresaImovel');
+            $table->string('paisEndereco');
+            $table->string('estadoEndereco');
+            $table->string('cidadeEndereco');
+            $table->string('logradouroEndereco');
+            $table->string('bairroEndereco');
+            $table->integer('numeroEndereco');
+            $table->string('cepEndereco');
+            $table->string('pontoReferenciaEndereco');
+            $table->string('imgImovel');
+            $table->float('preco');
+            $table->integer('qtdQuartos');
+            $table->integer('qtdJanelas');
+            $table->integer('qtdlampadas');
+            $table->integer('qtdBanheiro');
+            $table->enum('garagem',['sim','não']);
+            $table->enum('tipo',['apartamento','casa','kitnet','galpão']);
+            $table->enum('finalidade',['venda','locação']);
             $table->timestamps();
         });
     }
@@ -26,6 +45,6 @@ class CreateImovelsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('imovels');
+        Schema::dropIfExists('imoveis');
     }
 }
